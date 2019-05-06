8d1bb2c95134d6b2b9efb3951250a3ab61e5a7ea
An Le <an.le@notabasement.com>
2019-05-06 20:44:08 +0700

    Initialize project

---
+++ NewYourTimes_Demo/NewYourTimes_Demo/Source/UI/Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewYourTimes_Demo
+//
+//  Created by An Le  on 5/6/19.
+//  Copyright © 2019 An Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
