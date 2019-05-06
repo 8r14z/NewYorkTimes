23fc6d41b27982b9ac375f069c30ce376b4a78ce
An Le <an.le@notabasement.com>
2019-05-07 01:19:21 +0700

    md5

--- NewYourTimes_Demo/NewYourTimes_Demo/Source/Common/Utility/String+Utility.swift
+++ NewYourTimes_Demo/NewYourTimes_Demo/Source/Common/Utility/String+Utility.swift
@@ -7,12 +7,30 @@
 //
 
 import Foundation
+import CommonCrypto
 
 
 
 extension String {
     
+    /// "Hello" => 8b1a9953c4611296a827abf8c47804d7
     func md5() -> String {
-        return ""
+        
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        let messageData = data(using:.utf8)!
+        var digestData = Data(count: length)
+        
+        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+            messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                    let messageLength = CC_LONG(messageData.count)
+                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                }
+                return 0
+            }
+        }
+        
+        return digestData.map { String(format: "%02hhx", $0) }.joined()
     }
 }
+
