3bdad54e6c66d152c93d2f49c334cb3b264028f8
An Le <an.le@notabasement.com>
2019-05-11 21:42:55 +0700

    update

--- NewYourTimes/NewYourTimesTests/Common/TestUtilities.swift
+++ NewYourTimes/NewYourTimesTests/Common/TestUtilities.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2019 An Le. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import NewYourTimes
+
+
+
+class TestUtilities: XCTestCase {
+    
+//    var iso8601DateString: String {
+//        let dateFormatter = DateFormatter(format: .iso8601Date)
+//        return dateFormatter.string(from: self)
+//    }
+//
+//    var iso8601DateAndTimeString: String {
+//        let dateFormatter = DateFormatter(format: .iso8601DateAndTime)
+//        return dateFormatter.string(from: self)
+//    }
+//}
+}
